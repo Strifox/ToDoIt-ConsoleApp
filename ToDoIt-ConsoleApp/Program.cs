fdcb6c8c90a92a10d380e18b4997740dbd2d55e5
Johan Svensson <johan.svensson93@outlook.com>
2021-10-11 15:10:35 +0200

    Add project files.

---
+++ ToDoIt-ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoIt_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

1a4bb623278369c1df887c868ab7d5d5bbb8a8df
Johan Svensson <johan.svensson93@outlook.com>
2021-10-13 12:19:17 +0200

    Added comments and todo class
    Refined code and added new class: Todo

--- ToDoIt-ConsoleApp/Model/Person.cs
+++ ToDoIt-ConsoleApp/Model/Person.cs
@@ -1,39 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ToDoIt_ConsoleApp.Data;
 
 namespace ToDoIt_ConsoleApp.Model
 {
     public class Person
     {
-        static int idCounter = 0;
-        private readonly int personId;
-        public int PersonId { get { return personId; } }
+        //static int idCounter = 0;
 
+        #region Private Fields
+
+        private readonly int personId;
         private string firstname;
+        private string lastname;
+
+        #endregion
+
+        #region Public Properties
+
+        public int PersonId { get => personId; }
         public string Firstname
         {
-            get { return firstname; }
+            get => firstname;
             set
             {
+                // Input can not be null or empty else throws new exception
                 if (string.IsNullOrWhiteSpace(value))
-                {
                     throw new ArgumentException("Firstname cannot be null or empty");
-                }
+
                 firstname = value;
             }
         }
-        private string lastname;
         public string Lastname
         {
-            get { return lastname; }
+            get => lastname;
             set
             {
+                // Input can not be null or empty else throws new exception
                 if (string.IsNullOrWhiteSpace(value))
                     throw new ArgumentException("Lastname cannot be null or empty");
+
                 lastname = value;
             }
         }
+
+        #endregion
 
 
         public Person(string firstname, string lastname)
---
+++ ToDoIt-ConsoleApp/Model/Todo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt_ConsoleApp.Model
+{
+    public class Todo
+    {
+        #region Private Fields
+
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        #endregion
+
+        #region Public Properties
+
+        //public int TodoId { get => todoId; }
+        public string Description { get => description; set => description = value; }
+        //public bool Done { get => done; set => done = value; }
+        //public Person Assignee { get => assignee; set => assignee = value; }
+
+        #endregion
+
+        public Todo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            Description = description;
+        }
+    }
+}

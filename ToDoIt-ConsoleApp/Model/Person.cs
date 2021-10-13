08ef74be866be34fcd587abd754cf4493e84ef0f
Johan Svensson <johan.svensson93@outlook.com>
2021-10-13 09:52:31 +0200

    Added Person class and person unit testing

---
+++ ToDoIt-ConsoleApp/Model/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoIt_ConsoleApp.Model
+{
+    public class Person
+    {
+        static int idCounter = 0;
+        private readonly int personId;
+        public int PersonId { get { return personId; } }
+
+        private string firstname;
+        public string Firstname
+        {
+            get { return firstname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Firstname cannot be null or empty");
+                }
+                firstname = value;
+            }
+        }
+        private string lastname;
+        public string Lastname
+        {
+            get { return lastname; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Lastname cannot be null or empty");
+                lastname = value;
+            }
+        }
+
+
+        public Person(string firstname, string lastname)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+        }
+    }
+}
---
+++ UnitTest/PersonClassTests.cs
@@ -0,0 +1,27 @@
+using System;
+using ToDoIt_ConsoleApp.Model;
+using Xunit;
+
+namespace UnitTest
+{
+    public class PersonClassTests
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            Person person = new Person("Erik", "Karlsson");
+
+            Assert.NotNull(person);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void FirstnameTest(string firstname)
+        {
+            var result = Assert.Throws<ArgumentException>(() => new Person(firstname, "Karlsson"));
+
+            Assert.Equal("Firstname cannot be null or empty", result.Message);
+        }
+    }
+}
